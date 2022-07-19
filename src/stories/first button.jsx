80a1ea8bdf067fe625c7e41bf78aff43524ffca9
BurakAliksei <91836874+BurakAliksei@users.noreply.github.com>
2022-07-19 18:00:01 +0300

    cannot understand where is my button

--- src/stories/first button.jsx
+++ src/stories/first button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './first button.css';
 
-export const myButton = ({ primary, backgroundColor, size, ...props }) => {
+export const myButton = ({ primary, backgroundColor, size, label, ...props }) => {
     const mode = primary ? 'my-button--primary' : 'my-button--secondary';
     return (
       <button
@@ -10,7 +10,7 @@ export const myButton = ({ primary, backgroundColor, size, ...props }) => {
         className={['my-button', `my-button--${size}`, mode].join(' ')}
         style={backgroundColor && { backgroundColor }}
         {...props}
-      >  Click me
+      >  {label}
       </button>
     );
   };
--- src/stories/first button.stories.jsx
+++ src/stories/first button.stories.jsx
@@ -15,17 +15,21 @@ export default {
   
   Primary.args = {
     primary: false,
+    label: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
+  label: 'Button',
 };
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
+  label: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
+  label: 'Button',
 };
